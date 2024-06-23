0f475f13b18810d9d5e412b78c2b7a8771f14a7a
Guilherme Souza Grande <gsouzagrande@gmail.com>
2024-06-23 18:09:17 +0000

    Add routes to Account and Events page

--- eventoken/routes/index.js
+++ eventoken/routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Eventoken' });
 });
 
+/* GET Account page. */
+router.get('/account', function(req, res, next) {
+  res.render('account', { title: 'Eventoken - Account', name:null });
+});
+
+/* GET Events page. */
+router.get('/events', function(req, res, next) {
+  res.render('events', { title: 'Eventoken - Events', name:null });
+});
+
 module.exports = router;
