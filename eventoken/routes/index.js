324e864435519f7d239104b6fe1753431abb0908
Guilherme Souza Grande <gsouzagrande@gmail.com>
2024-06-25 18:10:47 +0000

    Add new GET and POST routes to adduser

--- eventoken/routes/index.js
+++ eventoken/routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const User = require('../models/user');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +15,45 @@ router.get('/account', function(req, res, next) {
 /* GET Events page. */
 router.get('/events', function(req, res, next) {
   res.render('events', { title: 'Eventoken - Events', name:null });
+});
+
+/* GET Events page. */
+router.get('/events', function(req, res, next) {
+  res.render('events', { title: 'Eventoken - Events', name:null });
+});
+
+/* GET and POST Manag - adduser page. */
+router.get('/manag/adduser', function(req, res, next) {
+  res.render('manag/adduser', { title: 'Eventoken - Add User' });
+});
+
+router.post('/manag/adduser', async (req, res) => {
+  const { userMetamaskAdr, userNickname } = req.body;
+  try {
+    // Check if the user already exist useing the MetaMask address.
+    let user = await User.findOne({ userMetamaskAdr });
+
+    if (user) {
+      console.log("User already exist")
+      // redirect to account page.
+      res.redirect('/account');
+    } else {
+      // If the user don't exist, create a new one.
+      const newUser = await User.create({
+        userMetamaskAdr,
+        userID: 'useridtest01', // UserID example
+        userNickname,
+        userName: 'TestUser', // Name example
+        userCity: 'Test City', // City example
+        userEmail: 'test@example.com' // Email example
+      });
+      console.log("New user created")
+      // redirect to account page.
+      res.redirect('/account');
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
 
 module.exports = router;
