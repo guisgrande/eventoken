d5e12f914fe1990664c3a8fa72615ca93c9e4dfc
Guilherme Souza Grande <gsouzagrande@gmail.com>
2024-06-25 18:27:35 +0000

    Edit route to find user in the database

--- eventoken/routes/users.js
+++ eventoken/routes/users.js
@@ -3,12 +3,18 @@ var router = express.Router();
 const User = require('../models/user');
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
+router.get('/', async (req, res) => {
+  const { userMetamaskAdr } = req.query;
+  
   try {
-    const users = await User.find();
-    res.status(200).json(users);
+      const user = await User.findOne({ userMetamaskAdr });
+      if (user) {
+          res.status(200).json(user);
+      } else {
+          res.status(404).json({ message: 'User not found' });
+      }
   } catch (error) {
-    res.status(500).json({ message: error.message });
+      res.status(500).json({ message: error.message });
   }
 });
 
